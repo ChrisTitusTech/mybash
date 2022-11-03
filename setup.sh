266b8b5f4794a945e8b316d4ad2bc8deaf5ac445
Nicholas Anderson <Nicholasaa@gmail.com>
2022-11-02 19:46:59 -0700

    updated to Choose between Package managers and Super user groups.

--- setup.sh
+++ setup.sh
@@ -5,48 +5,66 @@ RED='\e[31m'
 YELLOW='\e[33m'
 GREEN='\e[32m'
 
-checkEnv(){
+checkEnv() {
+    ## Check Package Handeler
+    PACKAGEMANAGER='apt dnf'
+    for pgm in ${PACKAGEMANAGER}; do
+        if which ${pgm} >/dev/null; then
+            PACKAGER=${pgm}
+            echo -e "Using ${pgm}"
+        fi
+    done
+
     ## Check if the current directory is writable.
     GITPATH="$(dirname "$(realpath "$0")")"
-    if [[ ! -w ${GITPATH} ]];then
+    if [[ ! -w ${GITPATH} ]]; then
         echo -e "${RED}Can't write to ${GITPATH}${RC}"
         exit 1
     fi
 
     ## Check for requirements.
-    REQUIREMENTS='curl dnf groups sudo'
-    if ! which ${REQUIREMENTS}>/dev/null;then
+    REQUIREMENTS='curl groups sudo'
+    if ! which ${REQUIREMENTS} >/dev/null; then
         echo -e "${RED}To run me, you need: ${REQUIREMENTS}${RC}"
         exit 1
     fi
 
+    ## Check SuperUser Group
+    SUPERUSERGROUP='wheel sudo'
+    for sug in ${SUPERUSERGROUP}; do
+        if which ${sug} >/dev/null; then
+            SUGROUP=${sug}
+            echo -e "Super user group ${SUGROUP}"
+        fi
+    done
+
     ## Check if member of the sudo group.
-    if ! groups|grep wheel>/dev/null;then
+    if ! groups | grep ${SUGROUP} >/dev/null; then
         echo -e "${RED}You need to be a member of the sudo group to run me!"
         exit 1
     fi
 }
 
-installDepend(){
+installDepend() {
     ## Check for dependencies.
     DEPENDENCIES='autojump bash bash-completion'
     echo -e "${YELLOW}Installing dependencies...${RC}"
-    sudo dnf install -yq ${DEPENDENCIES}
+    sudo ${PACKAGER} install -yq ${DEPENDENCIES}
 }
 
-installStarship(){
-    if ! curl -sS https://starship.rs/install.sh|sudo sh;then
+installStarship() {
+    if ! curl -sS https://starship.rs/install.sh | sudo sh; then
         echo -e "${RED}Something went wrong during starship install!${RC}"
         exit 1
     fi
 }
 
-linkConfig(){
+linkConfig() {
     ## Check if a bashrc file is already there.
     OLD_BASHRC="${HOME}/.bashrc"
-    if [[ -e ${OLD_BASHRC} ]];then
+    if [[ -e ${OLD_BASHRC} ]]; then
         echo -e "${YELLOW}Moving old bash config file to ${HOME}/.bashrc.bak${RC}"
-        if ! mv ${OLD_BASHRC} ${HOME}/.bashrc.bak;then
+        if ! mv ${OLD_BASHRC} ${HOME}/.bashrc.bak; then
             echo -e "${RED}Can't move the old bash config file!${RC}"
             exit 1
         fi
@@ -61,7 +79,7 @@ linkConfig(){
 checkEnv
 installDepend
 installStarship
-if linkConfig;then
+if linkConfig; then
     echo -e "${GREEN}Done!\nrestart your shell to see the changes.${RC}"
 else
     echo -e "${RED}Something went wrong!${RC}"
