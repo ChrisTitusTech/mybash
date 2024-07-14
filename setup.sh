e0aed31746762d35bd47b5e380da56fa83eab3f7
Chris Titus <contact@christitus.com>
2024-07-13 21:45:06 -0500

    convert to sh and add fastfetch config

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-RC='\e[0m'
-RED='\e[31m'
-YELLOW='\e[33m'
-GREEN='\e[32m'
+RC='\033[0m'
+RED='\033[31m'
+YELLOW='\033[33m'
+GREEN='\033[32m'
 
 # Check if the home directory and linuxtoolbox folder exist, create them if they don't
 LINUXTOOLBOXDIR="$HOME/linuxtoolbox"
 
-if [[ ! -d "$LINUXTOOLBOXDIR" ]]; then
-    echo -e "${YELLOW}Creating linuxtoolbox directory: $LINUXTOOLBOXDIR${RC}"
+if [ ! -d "$LINUXTOOLBOXDIR" ]; then
+    echo "${YELLOW}Creating linuxtoolbox directory: $LINUXTOOLBOXDIR${RC}"
     mkdir -p "$LINUXTOOLBOXDIR"
-    echo -e "${GREEN}linuxtoolbox directory created: $LINUXTOOLBOXDIR${RC}"
+    echo "${GREEN}linuxtoolbox directory created: $LINUXTOOLBOXDIR${RC}"
 fi
 
-if [[ ! -d "$LINUXTOOLBOXDIR/mybash" ]]; then
-    echo -e "${YELLOW}Cloning mybash repository into: $LINUXTOOLBOXDIR/mybash${RC}"
+if [ ! -d "$LINUXTOOLBOXDIR/mybash" ]; then
+    echo "${YELLOW}Cloning mybash repository into: $LINUXTOOLBOXDIR/mybash${RC}"
     git clone https://github.com/ChrisTitusTech/mybash "$LINUXTOOLBOXDIR/mybash"
-    if [[ $? -eq 0 ]]; then
-        echo -e "${GREEN}Successfully cloned mybash repository${RC}"
+    if [ $? -eq 0 ]; then
+        echo "${GREEN}Successfully cloned mybash repository${RC}"
     else
-        echo -e "${RED}Failed to clone mybash repository${RC}"
+        echo "${RED}Failed to clone mybash repository${RC}"
         exit 1
     fi
 fi
@@ -34,58 +34,61 @@ command_exists() {
 checkEnv() {
     ## Check for requirements.
     REQUIREMENTS='curl groups sudo'
-    if ! command_exists ${REQUIREMENTS}; then
-        echo -e "${RED}To run me, you need: ${REQUIREMENTS}${RC}"
-        exit 1
-    fi
-
-    ## Check Package Handeler
-    PACKAGEMANAGER='apt yum dnf pacman zypper'
-    for pgm in ${PACKAGEMANAGER}; do
-        if command_exists ${pgm}; then
-            PACKAGER=${pgm}
-            echo -e "Using ${pgm}"
+    for req in $REQUIREMENTS; do
+        if ! command_exists $req; then
+            echo "${RED}To run me, you need: $REQUIREMENTS${RC}"
+            exit 1
         fi
     done
 
-    if [ -z "${PACKAGER}" ]; then
-        echo -e "${RED}Can't find a supported package manager"
+    ## Check Package Handler
+    PACKAGEMANAGER='apt yum dnf pacman zypper'
+    for pgm in $PACKAGEMANAGER; do
+        if command_exists $pgm; then
+            PACKAGER=$pgm
+            echo "Using $pgm"
+            break
+        fi
+    done
+
+    if [ -z "$PACKAGER" ]; then
+        echo "${RED}Can't find a supported package manager${RC}"
         exit 1
     fi
 
     ## Check if the current directory is writable.
-    GITPATH="$(dirname "$(realpath "$0")")"
-    if [[ ! -w ${GITPATH} ]]; then
-        echo -e "${RED}Can't write to ${GITPATH}${RC}"
+    GITPATH=`dirname \`realpath $0\``
+    if [ ! -w $GITPATH ]; then
+        echo "${RED}Can't write to $GITPATH${RC}"
         exit 1
     fi
 
     ## Check SuperUser Group
     SUPERUSERGROUP='wheel sudo root'
-    for sug in ${SUPERUSERGROUP}; do
-        if groups | grep ${sug}; then
-            SUGROUP=${sug}
-            echo -e "Super user group ${SUGROUP}"
+    for sug in $SUPERUSERGROUP; do
+        if groups | grep $sug >/dev/null; then
+            SUGROUP=$sug
+            echo "Super user group $SUGROUP"
+            break
         fi
     done
 
     ## Check if member of the sudo group.
-    if ! groups | grep ${SUGROUP} >/dev/null; then
-        echo -e "${RED}You need to be a member of the sudo group to run me!"
+    if ! groups | grep $SUGROUP >/dev/null; then
+        echo "${RED}You need to be a member of the sudo group to run me!${RC}"
         exit 1
     fi
-
 }
 
 installDepend() {
     ## Check for dependencies.
     DEPENDENCIES='bash bash-completion tar tree multitail fastfetch tldr trash-cli'
-    echo -e "${YELLOW}Installing dependencies...${RC}"
-    if [[ $PACKAGER == "pacman" ]]; then
+    echo "${YELLOW}Installing dependencies...${RC}"
+    if [ "$PACKAGER" = "pacman" ]; then
         if ! command_exists yay && ! command_exists paru; then
             echo "Installing yay as AUR helper..."
-            sudo ${PACKAGER} --noconfirm -S base-devel
-            cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git && sudo chown -R ${USER}:${USER} ./yay-git
+            sudo $PACKAGER --noconfirm -S base-devel
+            cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git && sudo chown -R $USER:$USER ./yay-git
             cd yay-git && makepkg --noconfirm -si
         else
             echo "Aur helper already installed"
@@ -98,9 +101,9 @@ installDepend() {
             echo "No AUR helper found. Please install yay or paru."
             exit 1
         fi
-        ${AUR_HELPER} --noconfirm -S ${DEPENDENCIES}
+        $AUR_HELPER --noconfirm -S $DEPENDENCIES
     else
-        sudo ${PACKAGER} install -yq ${DEPENDENCIES}
+        sudo $PACKAGER install -yq $DEPENDENCIES
     fi
 }
 
@@ -111,7 +114,7 @@ installStarship() {
     fi
 
     if ! curl -sS https://starship.rs/install.sh | sh; then
-        echo -e "${RED}Something went wrong during starship install!${RC}"
+        echo "${RED}Something went wrong during starship install!${RC}"
         exit 1
     fi
     if command_exists fzf; then
@@ -129,13 +132,13 @@ installZoxide() {
     fi
 
     if ! curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh; then
-        echo -e "${RED}Something went wrong during zoxide install!${RC}"
+        echo "${RED}Something went wrong during zoxide install!${RC}"
         exit 1
     fi
 }
 
 install_additional_dependencies() {
-    case $(command -v apt || command -v zypper || command -v dnf || command -v pacman) in
+    case `command -v apt || command -v zypper || command -v dnf || command -v pacman` in
         *apt)
             curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
             chmod u+x nvim.appimage
@@ -164,21 +167,25 @@ install_additional_dependencies() {
 
 linkConfig() {
     ## Get the correct user home directory.
-    USER_HOME=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
+    USER_HOME=`getent passwd ${SUDO_USER:-$USER} | cut -d: -f6`
     ## Check if a bashrc file is already there.
-    OLD_BASHRC="${USER_HOME}/.bashrc"
-    if [[ -e ${OLD_BASHRC} ]]; then
-        echo -e "${YELLOW}Moving old bash config file to ${USER_HOME}/.bashrc.bak${RC}"
-        if ! mv ${OLD_BASHRC} ${USER_HOME}/.bashrc.bak; then
-            echo -e "${RED}Can't move the old bash config file!${RC}"
+    OLD_BASHRC="$USER_HOME/.bashrc"
+    if [ -e $OLD_BASHRC ]; then
+        echo "${YELLOW}Moving old bash config file to $USER_HOME/.bashrc.bak${RC}"
+        if ! mv $OLD_BASHRC $USER_HOME/.bashrc.bak; then
+            echo "${RED}Can't move the old bash config file!${RC}"
             exit 1
         fi
     fi
 
-    echo -e "${YELLOW}Linking new bash config file...${RC}"
+    if [ ! -d $USER_HOME/.config/fastfetch ]; then
+        mkdir -p $USER_HOME/.config/fastfetch
+    fi
+    echo "${YELLOW}Linking new bash config file...${RC}"
     ## Make symbolic link.
-    ln -svf ${GITPATH}/.bashrc ${USER_HOME}/.bashrc
-    ln -svf ${GITPATH}/starship.toml ${USER_HOME}/.config/starship.toml
+    ln -svf $GITPATH/config.jsonc $USER_HOME/.config/fastfetch/config.jsonc
+    ln -svf $GITPATH/.bashrc $USER_HOME/.bashrc
+    ln -svf $GITPATH/starship.toml $USER_HOME/.config/starship.toml
 }
 
 checkEnv
@@ -188,7 +195,7 @@ installZoxide
 install_additional_dependencies
 
 if linkConfig; then
-    echo -e "${GREEN}Done!\nrestart your shell to see the changes.${RC}"
+    echo "${GREEN}Done!\nrestart your shell to see the changes.${RC}"
 else
-    echo -e "${RED}Something went wrong!${RC}"
+    echo "${RED}Something went wrong!${RC}"
 fi
